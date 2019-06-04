df7353d93ddfdca5365a9025c61c26a434f4849d
Mau Reyes <reyes.marq@gmail.com>
2019-06-04 13:40:10 -0600

    reorganize the sign up component

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 
-class SignUp extends React.Component {
+import SignUpForm from './SignUpForm'
+
+class Signup extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       name: '',
@@ -11,77 +12,36 @@ class SignUp extends React.Component {
       password: '',
       password_confirmation: '',
       errors: {}
-    }
+    };
   }
 
-  handleInputChange = event => {
+  handleInputChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value
-    })
+    });
   }
 
   handleSubmit = async (event) => {
     event.preventDefault()
-    
     try {
-
-      const user = await this.props.registerUser(this.state)
-      
-      localStorage.setItem('user', JSON.stringify(user))
-      this.props.setAuthUser(user)
-      this.props.history.push('/')
-      
-    } catch (errors) {
-      
-      this.setState({ errors })
-      
-    }
-
+        const user = await this.props.registerUser(this.state)
+        localStorage.setItem('user', JSON.stringify(user))
+        this.props.setAuthUser(user)
+        this.props.history.push('/');
+      } catch (errors) {
+        this.setState({ errors });
+      }
   }
-  
+
   render() {
     return (
-      <div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/img/bg-girl.jpg)`}}>
-        <div className="card card-shadowed p-50 w-400 mb-0" style={{maxWidth: '100%'}}>
-          <h5 className="text-uppercase text-center">Register</h5>
-          <br />
-          <br />
-          <form className="form-type-material" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <input type="text" name="name" onChange={this.handleInputChange} className="form-control" placeholder="Username" />
-              {
-                this.state.errors['name'] &&
-                <small className="text-danger">{ this.state.errors['name'] }</small>
-              }
-            </div>
-            <div className="form-group">
-              <input type="text" name="email" onChange={this.handleInputChange} className="form-control" placeholder="Email address" />
-              {
-                this.state.errors['email'] &&
-                <small className="text-danger">{ this.state.errors['email'] }</small>
-              }
-            </div>
-            <div className="form-group">
-              <input type="password" name="password" onChange={this.handleInputChange} className="form-control" placeholder="Password" />
-              {
-                this.state.errors['password'] &&
-                <small className="text-danger">{ this.state.errors['password'] }</small>
-              }
-            </div>
-            <div className="form-group">
-              <input type="password" name="password_confirmation" onChange={this.handleInputChange} className="form-control" placeholder="Password (confirm)" />
-            </div>
-            <br />
-            <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
-          </form>
-          <hr className="w-30" />
-          <p className="text-center text-muted fs-13 mt-20">Already have an account?
-            <Link to="/login">Sign in</Link>
-          </p>
-        </div>
-      </div>
-    )
+      <SignUpForm
+        handleInputChange={this.handleInputChange}
+        handleSubmit={this.handleSubmit}
+        errors={this.state.errors}
+      />
+    );
   }
 }
 
-export default SignUp
+export default Signup
